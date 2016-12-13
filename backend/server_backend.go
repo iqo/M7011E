1f97d6babb861ff60107b3e5789ed942d3e6c97f
Sophia Caspár <sophiacaspar@gmail.com>
2016-12-13 13:03:05 +0100

    work plz

--- backend/server_backend.go
+++ backend/server_backend.go
@@ -269,10 +269,10 @@ func (l *loginDB) getToplist(w http.ResponseWriter, r *http.Request, ps httprout
 	db := l.connectToDB()
 
     if list == "rate" {
-        rows, res, err := db.Query("select p.photoId, p.name, p.thumbnail, sum(case when rate is null then 0 else rate end) as ratingSum from hat4cat.rating as r right join hat4cat.photos as p on p.photoId=r.photoId group by photoId order by ratingsum desc limit 0,9")
+        rows, res, err = db.Query("select p.photoId, p.name, p.thumbnail, sum(case when rate is null then 0 else rate end) as ratingSum from hat4cat.rating as r right join hat4cat.photos as p on p.photoId=r.photoId group by photoId order by ratingsum desc limit 0,9")
         checkError(w, err)
     } else if list == "comment" {
-        rows, res, err := db.Query("select p.photoId, p.name, p.thumbnail, count(comment) as noComments from hat4cat.comment as c right join hat4cat.photos as p on p.photoId=c.photoId group by photoId order by noComments desc limit 0,9")
+        rows, res, err = db.Query("select p.photoId, p.name, p.thumbnail, count(comment) as noComments from hat4cat.comment as c right join hat4cat.photos as p on p.photoId=c.photoId group by photoId order by noComments desc limit 0,9")
         checkError(w, err)
     } else {
         fmt.Println("Forbidden getToplist request")
